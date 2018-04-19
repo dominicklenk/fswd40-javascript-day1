d85cddc937b82ee088dab9e99134a9c062c17fca
dominicklenk <dominic_klenk@yahoo.at>
2018-04-19 10:26:34 +0200

    Done Basic exercise

---
+++ basic_exercises/js/script.js
@@ -0,0 +1,12 @@
+// alert("Hello my name is Jeremy"); it was wrong
+
+document.write("Hello my name is Jeremy" + " ");
+
+var age = 25;
+
+document.write("and I am" + " " + age + " " + "years old" + "<br>");
+
+
+player = ['Martin', 'Thomas', 'Peter', 'Mathias', 'Niki'];
+
+document.write(player[3]);
